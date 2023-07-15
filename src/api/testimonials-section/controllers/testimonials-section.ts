f65616862bd29d0fab6636eff3a045ed8f2cbe42
henzzo <henokgetachew500@gmail.com>
2023-07-15 08:36:22 +0300

    content setup

---
+++ src/api/testimonials-section/controllers/testimonials-section.ts
@@ -0,0 +1,7 @@
+/**
+ * testimonials-section controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::testimonials-section.testimonials-section');
---
+++ src/api/testimonials-section/routes/testimonials-section.ts
@@ -0,0 +1,7 @@
+/**
+ * testimonials-section router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::testimonials-section.testimonials-section');
