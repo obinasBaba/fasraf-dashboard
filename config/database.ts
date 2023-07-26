b35cad42cedf6fa106228d79e5ddeb52eaac1959
henzzo <henokgetachew500@gmail.com>
2023-07-26 15:16:08 +0300

    prog: added render.yml

--- config/database.ts
+++ config/database.ts
@@ -1,29 +1,39 @@
 import path from 'path';
+import {parse} from 'pg-connection-string';
+
 
 export default ({ env }) => {
   const client = env('DATABASE_CLIENT', 'sqlite');
+  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
 
   const connections = {
     postgres: {
+      /* connection: {
+         connectionString: env('DATABASE_URL'),
+         host: env('DATABASE_HOST', 'localhost'),
+         port: env.int('DATABASE_PORT', 5432),
+         database: env('DATABASE_NAME', 'fasraf'),
+         user: env('DATABASE_USERNAME', 'fasraf'),
+         password: env('DATABASE_PASSWORD', 'fasraf'),
+         ssl: env.bool('DATABASE_SSL', false) && {
+           key: env('DATABASE_SSL_KEY', undefined),
+           cert: env('DATABASE_SSL_CERT', undefined),
+           ca: env('DATABASE_SSL_CA', undefined),
+           capath: env('DATABASE_SSL_CAPATH', undefined),
+           cipher: env('DATABASE_SSL_CIPHER', undefined),
+           rejectUnauthorized: env.bool(
+             'DATABASE_SSL_REJECT_UNAUTHORIZED',
+             true
+           ),
+         },
+         schema: env('DATABASE_SCHEMA', 'public'),
+       },*/
       connection: {
-        connectionString: env('DATABASE_URL'),
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'fasraf'),
-        user: env('DATABASE_USERNAME', 'fasraf'),
-        password: env('DATABASE_PASSWORD', 'fasraf'),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool(
-            'DATABASE_SSL_REJECT_UNAUTHORIZED',
-            true
-          ),
-        },
-        schema: env('DATABASE_SCHEMA', 'public'),
+        host,
+        port,
+        database,
+        user,
+        password
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
     },
